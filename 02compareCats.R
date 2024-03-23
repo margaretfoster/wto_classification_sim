8146a4c012f706a4d90f0ebb4185ef22ed830452
Margaret Foster <m.jenkins.foster at gmail dot com>
2024-03-23 06:47:45 -0400

    Initial commit

---
+++ 02compareCats.R
@@ -0,0 +1,175 @@
+## Use each model to estimate a DF with the tags
+## at the end, compare the regression model
+## for each version 
+rm(list=ls())
+
+loadPkg=function(toLoad){
+    for(lib in toLoad){
+        if(! lib %in% installed.packages()[,1])
+            { install.packages(lib, repos='http://cran.rstudio.com/') }
+        suppressMessages( library(lib, character.only=TRUE) ) }}
+
+
+packs <- c('tidyr',
+           'quanteda',
+           'dplyr',
+           'tidyverse',
+           "readxl",
+           'textrecipes',
+           'rsample',
+           "discrim")
+
+engines <-  c('glmnet',
+              "tidymodels",
+              "naivebayes",
+              "kernlab",
+              "ranger",
+              'kknn')
+
+loadPkg(c(packs, engines))
+
+#############################
+## Load simulated data with ground truth
+#############################
+
+load("simulatedTextData.Rdata")
+## Training -test split
+
+## TO HERE: Figure out thresholds for split
+## Split at:
+## 1%, 5%, 10%, 25% of data; then within that
+## make training/test
+
+## Load compareSlices()
+## takes: simulated data,
+## slice percent
+## and a random seed
+## returns 8-entry list comparing:
+## (1) performance in the "tagged" slice
+## (2) "unsupervised" tags in the rest of the data
+
+source("compareSlices.R")
+source("makeROC.R")
+
+## 5% tagged
+
+sim_05 <- compareSlices(data= sims,
+                       slicepercent=.05,
+                       seed=2822)
+
+roc_05 <- makeROC(modelList=sim_05,
+                     thresh=5)
+
+## 10% "tagged"
+sim_1 <- compareSlices(data= sims,
+                       slicepercent=.1,
+                       seed=2822)
+
+roc_1 <- makeROC(modelList=sim_1,
+                     thresh=10)
+
+## 25% tagged
+sim_25 <- compareSlices(data= sims,
+                       slicepercent=.25,
+                       seed=2822)
+
+roc_25 <- makeROC(modelList=sim_25,
+                     thresh=25)
+
+## ROC for taged:
+
+head(roc_25)
+
+
+gg <- ggplot(rbind(roc_05,
+                   roc_1,
+                   roc_25),
+             aes(x = 1 - specificity,
+                 y = sensitivity,
+                 color=mod)) +
+    geom_path() +
+    geom_abline(lty = 3) +
+    coord_equal() +
+    theme_bw()+
+    facet_wrap(group~ level)
+
+gg
+
+ggsave(gg,
+       file="pooledGammaClassificationComparison.pdf") 
+
+### Low vs high Gamme condition
+## 05:
+sim_05_highG <- list()
+sim_05_lowG <- list()
+for(i in 1:length(sim_05)){
+    sim_05_highG[[i]] <- sim_05[[i]][which(
+        sim_05[[i]]$gamma %in% c(0, 1)),]
+    sim_05_lowG[[i]] <- sim_05[[i]][which(
+            sim_05[[i]]$gamma %in% c(.25, .75)),] 
+}
+
+## 10
+sim_10_highG <- list()
+sim_10_lowG <- list()
+for(i in 1:length(sim_1)){
+    sim_10_highG[[i]] <- sim_1[[i]][which(
+        sim_1[[i]]$gamma %in% c(0, 1)),]
+    sim_10_lowG[[i]] <- sim_1[[i]][which(
+            sim_1[[i]]$gamma %in% c(.25, .75)),] 
+}
+
+##25
+sim_25_highG <- list()
+sim_25_lowG <- list()
+for(i in 1:length(sim_25)){
+    sim_25_highG[[i]] <- sim_25[[i]][which(
+        sim_25[[i]]$gamma %in% c(0, 1)),]
+    sim_25_lowG[[i]] <- sim_25[[i]][which(
+            sim_25[[i]]$gamma %in% c(.25, .75)),] 
+}
+
+## ROC Low
+
+sim_05_lowG_roc <-makeROC(modelList=sim_05_lowG,
+                          thresh=05)
+
+sim_10_lowG_roc <-makeROC(modelList=sim_10_lowG,
+                          thresh=10)
+
+sim_25_lowG_roc <-makeROC(modelList=sim_25_lowG,
+                          thresh=25)
+
+sim_05_highG_roc <-makeROC(modelList=sim_05_highG,
+                          thresh=05)
+
+sim_10_highG_roc <-makeROC(modelList=sim_10_highG,
+                          thresh=10)
+
+sim_25_highG_roc <-makeROC(modelList=sim_25_highG,
+                          thresh=25)
+
+
+ggdat2 <- rbind(
+    cbind(sim_05_highG_roc, cat="high frame separation"),
+        cbind(sim_10_highG_roc, cat="high frame separation"),
+    cbind(sim_25_highG_roc, cat="high frame separation"),
+    cbind(sim_05_lowG_roc, cat="low frame separation"),
+    cbind(sim_10_lowG_roc, cat="low frame separation"),
+    cbind(sim_25_lowG_roc, cat="low frame separation"))
+
+gg2 <- ggplot(ggdat2,
+             aes(x = 1 - specificity,
+                 y = sensitivity,
+                 color=mod,
+                 linetype=cat)) +
+    geom_path() +
+    geom_abline(lty = 3) +
+    coord_equal() +
+    theme_bw()+
+    facet_wrap(group~ level)
+
+gg2
+
+ggsave(gg2,
+       file="modelComparisonSeparatedGammas.pdf")
---
+++ compareSlices.R
@@ -0,0 +1,221 @@
+compareSlices <- function(data, slicepercent, seed){
+
+    set.seed(seed)
+    
+    ## Split data into "tagged" and "untagged"
+    ## to simulate having a corpus with a subset tagged
+    nRs <- dim(sims)[1]*slicepercent
+    
+    rs <- sample(x=sims$id,
+                 size=nRs,
+                 replace=FALSE)
+    
+    tagged <- sims[sims$id %in% rs,]
+    unsupervised <-sims[!(sims$id %in% rs), ]
+
+    print(colnames(tagged))
+    
+    ## Training-Test in "Tagged" Slice
+    ## Consistently 70/30
+    tagged_split <- initial_split(data = tagged,
+                                  strata = Frame,
+                                  prop = .7)
+    
+    tagged_train <- training(tagged_split)
+    tagged_test <- testing(tagged_split)
+    
+    ##%%%%%%%%%%%%%%%%%%%%%%%
+    ## Prep global settings for models
+    ##%%%%%%%%%%%%%%%%%%%%%%%
+    ## ID the columns for analysis + ID
+    
+    sim_rec <- recipe(Frame ~ texts + id,
+                      data = tagged_train) %>% 
+                          update_role(id,
+                                      new_role = "ID") 
+
+    ## Clean and convert to dtm
+    sim_rec <- sim_rec %>%
+        step_tokenize(texts) %>%
+            step_stopwords(texts) %>%
+                step_tokenfilter(texts) %>%
+                    step_tfidf(texts)
+
+    ##%%%%%%%%%%%%%%%%%%
+    ## Naive Bayes
+    ##%%%%%%%%%%%%%%%%%
+    set.seed(seed)
+    nb_spec <- naive_Bayes() %>%
+        set_mode("classification") %>%
+            set_engine("naivebayes")
+    
+    nb_workflow <- workflow() %>%
+        add_recipe(sim_rec) %>%
+            add_model(nb_spec)
+    
+    nb_fit <- nb_workflow %>%
+        fit(data = tagged_train)
+
+    print("start NB")
+    
+    ## Predict on Tagged Subset:
+    nb.pred <- predict(nb_fit, tagged_test)
+    tagged_nb_aug <- augment(nb_fit, tagged_test)
+    tagged_nb_aug$Frame <- as.factor(tagged_nb_aug$Frame)
+    tagged_nb_aug$mod <- "NB"
+    tagged_nb_aug$group <- "tagged"
+    
+    ## Scale to "untagged" part:
+    nb_pred_all <- predict(nb_fit, unsupervised)
+    sim_nb_aug <- augment(nb_fit, unsupervised)
+    sim_nb_aug$mod <- "NB"
+    sim_nb_aug$group <- "predict"
+
+    ##%%%%%%%%%%%%%%%%%%
+    ## Random Forest
+    ##%%%%%%%%%%%%%%%%%%
+    print("start RF")
+    
+    ## Structure:
+    set.seed(seed)
+    rf_spec <- rand_forest() %>%
+        set_mode("classification") %>%
+            set_engine("ranger")
+    
+    rf_workflow <- workflow() %>%
+        add_recipe(sim_rec) %>%
+            add_model(rf_spec)
+    
+    ## train:
+    rf_fit <- rf_workflow %>%
+        fit(data = tagged_train)
+    
+    ## Predict on Tagged Subset:
+    rf_pred <- predict(rf_fit, tagged_test)
+    tagged_rf_aug <- augment(rf_fit, tagged_test)
+    tagged_rf_aug$Frame <- as.factor(tagged_rf_aug$Frame)
+    tagged_rf_aug$mod <- "RF"
+    tagged_rf_aug$group <- "tagged"    
+
+    ## Scale to untaged
+    rf_pred.all <- predict(rf_fit, unsupervised)
+    sim_rf_aug <- augment(rf_fit, unsupervised)
+    sim_rf_aug$mod <- "RF"
+    sim_rf_aug$group <- "predict"
+
+    ##%%%%%%%%%%%%%%%%%%
+    ## SVM
+    ##%%%%%%%%%%%%%%%%%%
+    print("Start SVM")
+    
+    ## structure:
+    set.seed(seed)
+    svm_spec <- svm_poly() %>%
+        set_mode("classification") %>%
+            set_engine("kernlab")
+    
+    svm_workflow <- workflow() %>%
+        add_recipe(sim_rec) %>%
+            add_model(svm_spec)
+    
+    svm_fit <- svm_workflow %>%
+        fit(data = tagged_train)
+    
+    ## Predict on Tagged Subset:
+    svm_pred <- predict(svm_fit, tagged_test)
+    tagged_svm_aug <- augment(svm_fit, tagged_test)
+    tagged_svm_aug$Frame <- as.factor(tagged.svm.aug$Frame)
+    tagged_svm_aug$mod <- "SVM"
+    tagged_svm_aug$group <- "tagged"
+
+    ## Scale:
+    svm_pred.all <- predict(svm_fit, unsupervised)
+    sim_svm_aug <- augment(svm_fit, unsupervised)
+    sim_svm_aug$mod <- "SVM"
+    sim_svm_aug$group <- "predict"
+
+    ##%%%%%%%%%%%%%%%%%%
+    ## Logistic Reg with LASSO
+    ##%%%%%%%%%%%%%%%%%%
+    ## penalty: mixture =1 is LASSO
+    ## mixture =0 is ridge
+
+    print("start GLM")
+    
+    glm_spec <- multinom_reg(mixture=double(1),
+                             mode="classification",
+                             engine= "glmnet",
+                             penalty=0)
+    
+    glm_workflow <- workflow() %>%
+        add_recipe(sim_rec) %>%
+            add_model(glm_spec)
+    
+    glm_fit <- glm_workflow %>%
+        fit(data = tagged_train)
+    
+    ## Predict on Tagged Subset:
+    glm_pred <- predict(glm_fit, tagged_test)
+    tagged_glm_aug <- augment(glm_fit, tagged_test)    
+    tagged_glm_aug$Frame <- as.factor(tagged.glm.aug$Frame)
+    tagged_glm_aug$mod <- "GLM"
+    tagged_glm_aug$group <- "tagged"
+
+    ## Scale to unsupervised portion
+    ## GLM + LASSO
+    glm_pred.all <- predict(glm_fit, unsupervised)
+    sim_glm_aug <- augment(glm_fit, unsupervised)
+    sim_glm_aug$mod <- "GLM"
+    sim_glm_aug$group <- "predict"
+    ##%%%%%%%%%%%%%%%%%%
+    ## K Nearest Neighbors
+    ##%%%%%%%%%%%%%%%%%%
+    ## penalty: mixture =1 is LASSO
+    ## mixture =0 is ridge
+
+    print("start KNN")
+    
+    knn_spec <- nearest_neighbor(
+        mode = "classification",
+        engine = "kknn",
+        neighbors = 10,
+        )
+
+    knn_workflow <- workflow() %>%
+        add_recipe(sim_rec) %>%
+            add_model(knn_spec)
+    
+    knn_fit <- knn_workflow %>%
+        fit(data = tagged_train)
+    
+    ## Predict on Tagged Subset:
+    knn.pred <- predict(knn_fit, tagged_test)
+    tagged_knn_aug <- augment(knn_fit, tagged_test)    
+    tagged_knn_aug$Frame <- as.factor(tagged_knn_aug$Frame)
+    tagged_knn_aug$mod <- "KNN"
+    tagged_knn_aug$group <- "tagged"
+
+    ## Scale to unsupervised portion
+    ## KNN
+    knn_pred_all <- predict(knn_fit, unsupervised)
+    sim_knn_aug <- augment(knn_fit, unsupervised)
+    sim_knn_aug$mod <- "KNN"
+    sim_knn_aug$group <- "predict"
+    
+    ##%%%%%%%%%%%%%%%%%%%%%%%%%
+    ## Send data out
+    ##%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    ## Assemble data:
+    return(outlist=list(
+               perfSubsetGLM= tagged_glm_aug,
+               perfSubsetNB= tagged_nb_aug,
+               perfSubsetRF= tagged_rf_aug,
+               perfSubsetSVM= tagged_svm_aug,
+               predictedGLM = sim_glm_aug,
+               predictedNB = sim_nb_aug,
+               predictedRF = sim_rf_aug,
+               predictedSVM = sim_svm_aug,
+               predictedKNN = tagged_knn_aug,
+               predictedKNN = sim_knn_aug))
+}
