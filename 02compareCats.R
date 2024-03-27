04fb94066686e41d58fe0feca0eefdea645976d5
Margaret Foster <m.jenkins.foster at gmail dot com>
2024-03-27 11:05:14 -0400

    Updated results images

--- 02compareCats.R
+++ 02compareCats.R
@@ -1,7 +1,12 @@
 ## Use each model to estimate a DF with the tags
 ## at the end, compare the regression model
 ## for each version 
+
 rm(list=ls())
+
+setwd("~/Dropbox/WTO_Classification_Sim/")
+
+output <- "./output/"
 
 loadPkg=function(toLoad){
     for(lib in toLoad){
@@ -33,9 +38,8 @@ loadPkg(c(packs, engines))
 #############################
 
 load("simulatedTextData.Rdata")
-## Training -test split
 
-## TO HERE: Figure out thresholds for split
+## Training -test split
 ## Split at:
 ## 1%, 5%, 10%, 25% of data; then within that
 ## make training/test
@@ -60,6 +64,12 @@ sim_05 <- compareSlices(data= sims,
 roc_05 <- makeROC(modelList=sim_05,
                      thresh=5)
 
+first_roc <- sim_05[[1]] %>% ## GLM + Ridge
+  mutate(across(c(Frame), factor)) %>%
+  roc_curve(truth = Frame,
+            .pred_A,
+            event_level= "first")
+
 ## 10% "tagged"
 sim_1 <- compareSlices(data= sims,
                        slicepercent=.1,
@@ -76,14 +86,18 @@ sim_25 <- compareSlices(data= sims,
 roc_25 <- makeROC(modelList=sim_25,
                      thresh=25)
 
-## ROC for taged:
+## ROC for tagged:
 
 head(roc_25)
 
+ggdat1 <- rbind(roc_05,
+                roc_1,
+                roc_25)
+      
+ggdat1$group <- toupper(ggdat1$group)
 
-gg <- ggplot(rbind(roc_05,
-                   roc_1,
-                   roc_25),
+
+gg <- ggplot(ggdat1,
              aes(x = 1 - specificity,
                  y = sensitivity,
                  color=mod)) +
@@ -91,44 +105,63 @@ gg <- ggplot(rbind(roc_05,
     geom_abline(lty = 3) +
     coord_equal() +
     theme_bw()+
-    facet_wrap(group~ level)
+    ylab("Sensitivity")+
+    xlab("1 - Specificity")+
+    facet_wrap(level ~ group,
+               ncol=2) + 
+  labs(color = "Model")+
+  theme(legend.position="bottom")
 
 gg
 
 ggsave(gg,
-       file="pooledGammaClassificationComparison.pdf") 
+       height= 8,
+       width = 6,
+       file="pooledGammaClassificationComparison.png") 
 
 ### Low vs high Gamma condition
 ## 05:
 sim_05_highG <- list()
 sim_05_lowG <- list()
+sim_05_medG <- list()
+
 for(i in 1:length(sim_05)){
     sim_05_highG[[i]] <- sim_05[[i]][which(
-        sim_05[[i]]$gamma %in% c(0, 1)),]
+        sim_05[[i]]$gamma %in% c(0.01, 0.99)),]
     sim_05_lowG[[i]] <- sim_05[[i]][which(
-            sim_05[[i]]$gamma %in% c(.25, .75)),] 
+            sim_05[[i]]$gamma %in% c(.2, .8)),] 
+    sim_05_medG[[i]] <- sim_05[[i]][which(
+      sim_05[[i]]$gamma %in% c(.4, .6)),] 
 }
 
 ## 10
 sim_10_highG <- list()
 sim_10_lowG <- list()
+sim_10_medG <- list()
+
 for(i in 1:length(sim_1)){
-    sim_10_highG[[i]] <- sim_1[[i]][which(
-        sim_1[[i]]$gamma %in% c(0, 1)),]
-    sim_10_lowG[[i]] <- sim_1[[i]][which(
-            sim_1[[i]]$gamma %in% c(.25, .75)),] 
+  sim_10_highG[[i]] <- sim_1[[i]][which(
+    sim_1[[i]]$gamma %in% c(0.01, 0.99)),]
+  sim_10_lowG[[i]] <- sim_1[[i]][which(
+    sim_1[[i]]$gamma %in% c(.2, .8)),] 
+  sim_10_medG[[i]] <- sim_1[[i]][which(
+    sim_1[[i]]$gamma %in% c(.4, .6)),] 
 }
+
 
 ##25
 sim_25_highG <- list()
 sim_25_lowG <- list()
-for(i in 1:length(sim_25)){
-    sim_25_highG[[i]] <- sim_25[[i]][which(
-        sim_25[[i]]$gamma %in% c(0, 1)),]
-    sim_25_lowG[[i]] <- sim_25[[i]][which(
-            sim_25[[i]]$gamma %in% c(.25, .75)),] 
-}
+sim_25_medG <- list()
 
+for(i in 1:length(sim_25)){
+  sim_25_highG[[i]] <- sim_25[[i]][which(
+    sim_25[[i]]$gamma %in% c(0.01, 0.99)),]
+  sim_25_lowG[[i]] <- sim_25[[i]][which(
+    sim_25[[i]]$gamma %in% c(.2, .8)),] 
+  sim_25_medG[[i]] <- sim_25[[i]][which(
+    sim_25[[i]]$gamma %in% c(.4, .6)),] 
+}
 ## ROC Low
 
 sim_05_lowG_roc <-makeROC(modelList=sim_05_lowG,
@@ -152,7 +185,7 @@ sim_25_highG_roc <-makeROC(modelList=sim_25_highG,
 
 ggdat2 <- rbind(
     cbind(sim_05_highG_roc, cat="high frame separation"),
-        cbind(sim_10_highG_roc, cat="high frame separation"),
+    cbind(sim_10_highG_roc, cat="high frame separation"),
     cbind(sim_25_highG_roc, cat="high frame separation"),
     cbind(sim_05_lowG_roc, cat="low frame separation"),
     cbind(sim_10_lowG_roc, cat="low frame separation"),
@@ -167,9 +200,13 @@ gg2 <- ggplot(ggdat2,
     geom_abline(lty = 3) +
     coord_equal() +
     theme_bw()+
-    facet_wrap(group~ level)
+    facet_wrap(group~ level)+
+  labs(color = "Model", linetype="Gamma")+
+  theme(legend.position="bottom")
 
 gg2
 
-ggsave(gg2,
-       file="modelComparisonSeparatedGammas.pdf")
+ggsave(gg2, 
+       height= 6, 
+       width = 8,
+       file="modelComparisonSeparatedGammas.png")
--- makeROC.R
+++ makeROC.R
@@ -6,66 +6,70 @@ makeROC <- function(modelList, thresh){
     ## ROC -- in Tagged Set
     ## Removed hardcoded names for flexibility:
     
-    ## Could prob make into an arbitrary loop if wanted...
-    ## eg:  for(i in length(modelList){
-    ##     assign("roc.", i) <- modelList[[i]] %>%  ## GLM + LASSO
-    ##         roc_curve(truth = as.factor(Frame),
-    ##                   .pred_A,
-    ##                   event_level= "first")}
     
-    first_roc <- modelList[[1]] %>%  ## GLM + LASSO
-        roc_curve(truth = as.factor(Frame),
+    first_roc <- modelList[[1]] %>% ## GLM + Ridge
+      mutate(across(c(Frame), factor)) %>%
+        roc_curve(truth = Frame,
                   .pred_A,
                   event_level= "first")
     
     second_roc <- modelList[[2]] %>% ## NB
-    roc_curve(truth = as.factor(Frame),
+      mutate(across(c(Frame), factor)) %>%
+    roc_curve(truth = Frame,
               .pred_A,
               event_level= "first")
     
     third_roc <- modelList[[3]] %>% ##RF
-    roc_curve(truth = as.factor(Frame),
+      mutate(across(c(Frame), factor)) %>%
+    roc_curve(truth = Frame,
               .pred_A,
               event_level= "first")
     
     fourth_roc <- modelList[[4]] %>% ##SVM
-    roc_curve(truth = as.factor(Frame),
+      mutate(across(c(Frame), factor)) %>%
+    roc_curve(truth = Frame,
               .pred_A,
               event_level= "first")
     
     ninth_roc <- modelList[[9]] %>% ## KNN
-        roc_curve(truth = as.factor(Frame),
+      mutate(across(c(Frame), factor)) %>%
+        roc_curve(truth = Frame,
                   .pred_A,
               event_level= "first")    
 
     print("make predict")
     ## ROC -- in Predict Set
     fifth_pred_roc <- modelList[[5]] %>% ## GLM + LASSO Pred
-        roc_curve(truth = as.factor(Frame),
+      mutate(across(c(Frame), factor)) %>%
+        roc_curve(truth = Frame,
                   .pred_A,
                   event_level= "first")
     
     sixth_pred_roc <- modelList[[6]] %>% ## NB pred
-        roc_curve(truth = as.factor(Frame),
+      mutate(across(c(Frame), factor)) %>%
+        roc_curve(truth = Frame,
                   .pred_A,
                   event_level= "first")
 
     print("pred six")
     
     seventh_pred_roc <- modelList[[7]] %>% ## RF Pred
-        roc_curve(truth = as.factor(Frame),
+      mutate(across(c(Frame), factor)) %>%
+        roc_curve(truth = Frame,
                   .pred_A,
                   event_level= "first")
     
     eigth_pred_roc <- modelList[[8]] %>% ## SVM Pred
-        roc_curve(truth = as.factor(Frame),
+      mutate(across(c(Frame), factor)) %>%
+        roc_curve(truth = Frame,
                   .pred_A,
                   event_level= "first")
 
     print("pred 10")
     
     tenth_pred_roc <- modelList[[10]] %>%  ## KNN Pred
-        roc_curve(truth = as.factor(Frame),
+      mutate(across(c(Frame), factor)) %>%
+        roc_curve(truth = Frame,
                   .pred_A,
                   event_level= "first")
 
@@ -73,38 +77,38 @@ makeROC <- function(modelList, thresh){
     ##  Combine
     roc_all_mods <- rbind(
         cbind(first_roc,
-              mod=unique(modelList[[1]]$mod),
+              mod=unique(modelList[[1]]$model),
               group=unique(modelList[[1]]$group),
               level=thresh),
         cbind(second_roc,
-              mod=unique(modelList[[2]]$mod),
+              mod=unique(modelList[[2]]$model),
               group=unique(modelList[[2]]$group), level=thresh),
         cbind(third_roc,
-              mod=unique(modelList[[3]]$mod),
+              mod=unique(modelList[[3]]$model),
               group=unique(modelList[[3]]$group), level=thresh),
         cbind(fourth_roc,
-              mod=unique(modelList[[4]]$mod),
+              mod=unique(modelList[[4]]$model),
               group=unique(modelList[[4]]$group),level=thresh),
         cbind(fifth_pred_roc,
-              mod=unique(modelList[[5]]$mod),
+              mod=unique(modelList[[5]]$model),
               group=unique(modelList[[5]]$group), level=thresh),
         cbind(sixth_pred_roc,
-              mod=unique(modelList[[6]]$mod),
+              mod=unique(modelList[[6]]$model),
               group=unique(modelList[[6]]$group),
               level=thresh),
         cbind(seventh_pred_roc,
-              mod=unique(modelList[[7]]$mod),
+              mod=unique(modelList[[7]]$model),
               group=unique(modelList[[7]]$group),
               level=thresh),
         cbind(eigth_pred_roc,
-              mod=unique(modelList[[8]]$mod),
+              mod=unique(modelList[[8]]$model),
                group=unique(modelList[[8]]$group),level=thresh),
         cbind(ninth_roc,
-              mod=unique(modelList[[9]]$mod),
+              mod=unique(modelList[[9]]$model),
               group=unique(modelList[[9]]$group),
               level=thresh),
         cbind(tenth_pred_roc,
-              mod=unique(modelList[[10]]$mod),
+              mod=unique(modelList[[10]]$model),
               group=unique(modelList[[10]]$group),
               level=thresh))
 
